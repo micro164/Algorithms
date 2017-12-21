c92ab6c90551a1b37a66b6231a4e6b8f92abf533
micro164 <micro164@gmail.com>
2017-12-21 10:55:16 -0600

    Added Selection Sort Algorithm

---
+++ SelectionSort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+int* SELECTIONSORT(int a[], int n);
+
+int main(){
+    int n = 10;
+    int a[] = {};
+    for(int i = 0; i < 10; i++){
+        a[i] = rand()%10;
+    }
+
+    int *b = SELECTIONSORT(a, n);
+
+    for(int i = 0; i < 10; i++){
+        cout << b[i] << '\n';
+    }
+
+    return 0;
+}
+
+int* SELECTIONSORT(int a[], int n){
+  int smallest = 0;
+  int temp = 0;
+
+  for(int j = 0; j < n; j++){
+    smallest = j;
+    for(int i = j; i < n; i++){
+      if(a[i] < a[smallest]){
+        smallest = i;
+      }
+    }
+    temp = a[j];
+    a[j] = a[smallest];
+    a[smallest] = temp;
+  }
+
+  return a;
+}
